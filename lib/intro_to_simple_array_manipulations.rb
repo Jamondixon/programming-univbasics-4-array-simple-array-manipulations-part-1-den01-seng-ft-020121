c9a535f266df3a01b465224e2141dbbbad367335
Jamon Dixon <jamonadixon@gmail.com>
2021-01-15 04:03:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,8 +15,9 @@ def using_pop(continents)
   continents.pop
 end
 
-def pop_with_args(dog_breeds)
+def pop_with_args(dog_breeds, small_dogs)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  small_dogs = ["Chihuahua", "Shiba Inu"]
   dog_breeds.pop(2)
 end
 
