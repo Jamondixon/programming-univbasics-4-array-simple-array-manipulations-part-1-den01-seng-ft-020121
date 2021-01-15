b00cfda29809c73b91c87e4cdb3f157b286a01d6
Jamon Dixon <jamonadixon@gmail.com>
2021-01-15 03:50:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,7 +21,7 @@ def pop_with_args(dog_breeds, small_dogs)
   dog_breeds.pop
 end
 
-def using_shift(my_favorite_cities,)
+def using_shift(my_favorite_cities)
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   my_favorite_cities.shift
 end
