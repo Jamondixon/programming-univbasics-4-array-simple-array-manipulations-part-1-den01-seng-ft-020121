37e0c9aef7bbeb2128077a744b3846e1983333c5
Jamon Dixon <jamonadixon@gmail.com>
2021-01-15 03:49:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,3 +9,23 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
   new_neighborhood = "Staten Island"
   bouroughs_in_nyc.unshift(new_neighborhood)
 end
+
+def using_pop(continents)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
+end
+
+def pop_with_args(dog_breeds, small_dogs)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  small_dogs = ["Chihuahua", "Shiba Inu"]
+  dog_breeds.pop
+end
+
+def using_shift(my_favorite_cities,)
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  my_favorite_cities.shift
+end
+
+def shift_with_args
+
+end
